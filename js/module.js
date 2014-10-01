de314a5162cedf05348b2d21fd65b90808e4e621
johnldoner <johnldoner@gmail.com>
2014-09-30 21:05:13 -0400

    removed top auth

--- js/module.js
+++ js/module.js
@@ -8,29 +8,6 @@ app.controller("fluttrCtrl", function($scope, $firebase) {
 
 
 var ref = new Firebase("https://resplendent-fire-1812.firebaseio.com/ideas");
-var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-  if (user) {
-    console.log(user.accessToken);
-    console.log(user.displayName);
-    $scope.displayName = user.displayName;
-    $scope.id = user.id;
-  }
-});
-
-
-
-
-
-
-$scope.login = function() {
-	auth.login('facebook');	
-}
-
-$scope.logout = function() {
-	auth.logout();	
-}
-
-
 
 		  var auth = new FirebaseSimpleLogin(ref, function(error, user) {
 		  	if (!user) {
@@ -40,9 +17,11 @@ $scope.logout = function() {
 		  	}
 		  	else {
 		  		console.log(user.displayName);
+		  		console.log(user.uid)
 
-		  		var username = user.displayName;
-		  		
+		  		$scope.displayName = user.displayName;
+		  		$scope.uid = user.uid;
+
 		  	}
 						  			  	
 			});
@@ -54,26 +33,24 @@ $scope.ideas = sync.$asArray();
 
 
 $scope.title = "";
-
-
 $scope.desc = "";
 
-$scope.user = username;
-
-
 $scope.addIdea = function(title, desc, id, displayName) {
+
+$scope.displayName = user.displayName;
+$scope.uid = user.uid;
+
 	$scope.ideas.$add(
 		{
-			title: title, 
-			desc: desc,
-			user: user
+			"title": title, 
+			"desc": desc,
+			"user": "test"
 		}
 		);
 	//The user id needs to be pulled and we need to post
 	//the idea id generated here to the user branch
 	$scope.title = '';
 	$scope.desc = '';
-
 }
 
 
