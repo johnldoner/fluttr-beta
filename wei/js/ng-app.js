3a26cd1d237849982f8daa55822d9c9f45ff0649
johnldoner <johnldoner@gmail.com>
2014-10-29 18:28:18 -0400

    login function integrated with wei's code

--- wei/js/ng-app.js
+++ wei/js/ng-app.js
@@ -1,53 +1,54 @@
 
 var app = angular.module("fluttrApp", ["firebase"]);
 
-
-
 app.controller("fluttrCtrl", function($scope, $firebase) {
 
+	var ref = new Firebase("https://crowdfluttr.firebaseio.com/ideas");
+	var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+	  $scope.loginGoogle = function() {
+		auth.login('google', {
+		 // rememberMe: "sessiononly"
+		 rememberMe: true;
+		});
+		$scope.username = user.displayName;
+		$scope.userid = user.id;
+		$scope.gmail = user.email;
+		$scope.uniqueid = user.uid;
+		$scope.loggedIn = true;
+	  };
+	  $scope.loginFacebook = function() {
+		auth.login('facebook', {
+		 // rememberMe: "sessiononly"
+		 rememberMe: true;
+		});
+		$scope.username = user.displayName;
+		$scope.userid = user.id;
+		$scope.gmail = user.email;
+		$scope.uniqueid = user.uid;
+		$scope.loggedIn = true;
 
+	  };
+	  $scope.logout = function() {
+		ref.unauth();
+		$scope.loggedIn = false;
+	  };
+		$scope.addIdea = function(title, id, displayName) {
 
-var ref = new Firebase("https://crowdfluttr.firebaseio.com/ideas");
+		$scope.displayName = user.displayName;
+		$scope.uid = user.uid;
 
-
-
-		  var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-		  	if (!user) {
-				auth.login('google', {
-				  rememberMe: true
-				});
-		  	}
-		  	else {
-		  		console.log(user.displayName);
-		  		console.log(user.uid)
-
-		  		$scope.displayName = user.displayName;
-		  		$scope.uid = user.uid;
-
-				$scope.addIdea = function(title, id, displayName) {
-
-				$scope.displayName = user.displayName;
-				$scope.uid = user.uid;
-
-					$scope.ideas.$add(
-						{
-							"title": title, 
-							"user": user.uid,
-							"timestamp": Date.now()
-						}
-						);
-					//The user id needs to be pulled and we need to post
-					//the idea id generated here to the user branch
-					$scope.title = '';
+			$scope.ideas.$add(
+				{
+					"title": title, 
+					"user": user.uid,
+					"timestamp": Date.now()
 				}
-		  	}
-						  			  	
-			});
-
-var sync = $firebase(ref);
-
-
-$scope.ideas = sync.$asArray();
-
-
+				);
+			//The user id needs to be pulled and we need to post
+			//the idea id generated here to the user branch
+			$scope.title = '';
+		};
+		var sync = $firebase(ref);
+		$scope.ideas = sync.$asArray();
+	});
 });
