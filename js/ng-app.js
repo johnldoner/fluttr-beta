e2939aaca2bb0cec4044290ab13acf3c72c712d6
johnldoner <johnldoner@gmail.com>
2014-11-22 17:45:14 -0500

    login complete babyyyyy

--- js/ng-app.js
+++ js/ng-app.js
@@ -4,31 +4,6 @@ var app = angular.module("fluttrApp", ["firebase"]);
 app.controller("fluttrCtrl", function($scope, $firebase) {
 	var url = "https://crowdfluttr.firebaseio.com/";
 	var ref = new Firebase(url);
-	
-  	// LOGIN FUNCTION -- ENCAPSULATE EVERYTHING INSIDE THIS!!!
-  	$scope.loginGoogle = function() {
-	    console.log("Got into google login");
-	    ref.authWithOAuthPopup("google", function(error, authData) { 
-	    	$scope.loggedIn = true;
-	    	$scope.uniqueid = authData.google.displayName;
-	    	    	 }, {
-		  remember: "sessionOnly",
-		  scope: "email"
-		});
-
-	    $scope.loggedIn = true;
-
-		function checkLogin() {
-			ref.onAuth(function(authData) {
-			  if (authData) {
-			    // user authenticated with Firebase
-			    console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-			    window.location.href = "main.html";
-			  }
-			});
-		}
-		checkLogin();
-
 		$scope.addIdea = function(title, id, displayName) {
 			$scope.displayName = authData.displayName;
 			$scope.uid = authData.uid;
@@ -46,16 +21,5 @@ app.controller("fluttrCtrl", function($scope, $firebase) {
 		};
 		var sync = $firebase(ref);
 		$scope.ideas = sync.$asArray();
-
-
-
-	// END LOGIN FUNCTION -- MAKE SURE ALL CODE IS INSIDE THIS!!!
-	};
-
-	 $scope.logout = function() {
-		ref.unauth();
-		$scope.loggedIn = false;
-		window.location.href="/";
-	  };
 
 });
